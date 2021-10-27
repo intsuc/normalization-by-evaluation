c55c5598bc992dca94c24969d40a1b0eea98f876
intsuc <intsuc@gmail.com>
2021-10-27 17:40:48 +0900

    Use call-by-need evaluation

--- src/main/scala/Nbe.scala
+++ src/main/scala/Nbe.scala
@@ -1,5 +1,10 @@
 import scala.collection.mutable.WeakHashMap
 
+final class Thunk[A](_a: => A):
+  private lazy val a: A = _a
+
+  def get: A = a
+
 final class Sym private (id: Int)
 
 object Sym:
@@ -22,11 +27,11 @@ enum Exp:
 enum Val:
   case Var(key: Sym)
   case Abs(variables: Vars, key: Sym, body: Exp)
-  case App(operator: Val, operand: Val)
+  case App(operator: Val, operand: Thunk[Val])
   case Fun(variables: Vars, key: Sym, domain: Val, codomain: Exp)
   case Typ
 
-type Env = WeakHashMap[Sym, Val]
+type Env = WeakHashMap[Sym, Thunk[Val]]
 
 def freeVars(using env: Env): Exp => Vars =
   case Exp.Var(key)                   => if env.contains(key) then Set.empty else Set(key)
@@ -36,17 +41,17 @@ def freeVars(using env: Env): Exp => Vars =
   case Exp.Typ                        => Set.empty
   case Exp.Ann(annotated, _)          => freeVars(annotated)
 
-def call(using env: Env)(key: Sym, v: Val, e: Exp): Val =
+def call(using env: Env)(key: Sym, v: Thunk[Val], e: Exp): Val =
   env(key) = v
   eval(e)
 
 def eval(using env: Env): Exp => Val =
-  case Exp.Var(key)                         => env(key)
+  case Exp.Var(key)                         => env(key).get
   case Exp @ Exp.Abs(key, body)             => Val.Abs(freeVars(Exp), key, body)
   case Exp.App(operator, operand)           =>
     eval(operator) match
-      case Val.Abs(_, key, body) => call(key, eval(operand), body)
-      case operator              => Val.App(operator, eval(operand))
+      case Val.Abs(_, key, body) => call(key, Thunk(eval(operand)), body)
+      case operator              => Val.App(operator, Thunk(eval(operand)))
   case Exp @ Exp.Fun(key, domain, codomain) => Val.Fun(freeVars(Exp), key, eval(domain), codomain)
   case Exp.Typ                              => Val.Typ
   case Exp.Ann(annotated, _)                => eval(annotated)
